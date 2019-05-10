a1ac3b0bc8a99528d7eec9efe6a07bd7dbe93e55
Mafalda Pinheiro Luz <48191234+MafaldaPLuz@users.noreply.github.com>
2019-05-10 03:22:59 +0200

    Add files via upload

--- task.py
+++ task.py
@@ -1,43 +1,49 @@
-#
-#Flight simulator. 
-#Write a code in python that simulates the tilt correction of the plane (angle between plane wings and earth). 
-#The program should print out current orientation, and applied tilt correction. 
-#The program should run in infinite loop, until user breaks the loop. 
-#Assume that plane orientation in every new simulation step is random angle with gaussian distribution (the planes is experiencing "turbulations"). 
+import time
+from random import gauss
+import logging 
 
-#With every simulation step the orentation should be corrected, applied and printed out.
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
-import random
-from FlightSimulator import FlightSimulator
+class Plane:
+    def __init__(self):
+        self.angle = gauss(0,7)
+        self.correction = 0  
+        
+    def ajust(self, how_much):
+        self.correction = how_much
+        if(self.angle-how_much < 0):
+            self.angle += how_much
+        else:
+            self.angle -= how_much
+
+
+class Environment:
+    def __init__(self, plane):
+        self.plane= plane
+        
+    def flight(self):
+        degree = gauss(0, 3)
+        disturbance = degree
+        self.plane.angle += disturbance
+        self.plane.ajust(degree*0.75)
+        logging.info('The disturbance is ' + str(disturbance))
+                    
 
 def main():
-	flight = FlightSimulator()
-	zero = False
-	angle = flight.angle
-	
-	while(not zero):
-		
-		print("The angle of your plane is :"+str(flight.angle))
-		if(flight.angle == 0):
-			zero = True
-			print("your plane is ok")
-		else:
-			angle = flight.ajust(random.randint(1,3))
-			angle = flight.disturbance()
-			print("You ajusted the angle to :"+str(flight.angle))
-			
-		
- 
+    logging.basicConfig(filename = 'task.log',level = logging.DEBUG)
+    plane = Plane()
+    environment = Environment(plane)
+    zero = False
+    
+    while(not zero):
+        if(abs(plane.angle)==0):
+            zero = True
+            logging.info('The plane is stable')
+            
+        else:
+            environment.flight()
+            logging.info('The angle of the plane is ' + str(plane.angle))
+            time.sleep(2)    
+        
+
 if __name__== '__main__':
 	main()
+		
